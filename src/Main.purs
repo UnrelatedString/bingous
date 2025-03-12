3056bdcf7a36aaabe7d7886ffcb269736ea15ae7
UnrelatedString <unrelatedstring@gmail.com>
2025-03-12 15:10:11 -0400

    copied some stuff from the guide because this template is confusing aaaA

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,33 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.VDom.Driver (runUI)
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI component unit body
 
+component :: forall query input output m. H.Component query input output m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval H.defaultEval { handleAction = handleAction }
+    }
+
+type State = Unit
+type Action = Unit
+
+initialState :: forall input. input -> State
+initialState _ = unit
+
+handleAction :: forall output m. Action -> H.HalogenM State Action () output m Unit
+handleAction = pure
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render _ = HH.div_ []
