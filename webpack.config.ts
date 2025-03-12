a7c2dfe45bb2b350afc14219662692d0a58078c9
UnrelatedString <unrelatedstring@gmail.com>
2025-03-12 15:22:22 -0400

    blindly copying my webpack config from Eyes of Hell :p might get it actually set up after lunch

---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import webpack from 'webpack';
+import _ from 'webpack-dev-server'; // thanks https://github.com/DefinitelyTyped/DefinitelyTyped/issues/27570#issuecomment-437115227
+import CopyPlugin from "copy-webpack-plugin";
+
+const dist = path.resolve(__dirname, "dist");
+
+// thank you https://stackoverflow.com/a/71302165
+
+const config = (
+  _env: Record<string, any>,
+  _argv: Record<string, any>,
+): webpack.Configuration => ({
+  mode: "production",
+  entry: {
+    index: "./web/index.js"
+  },
+  output: {
+    path: dist,
+    filename: "[name].js",
+    clean: true
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    client: {
+      overlay: false,
+    },
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        path.resolve(__dirname, "web/static"),
+      ]
+    }),
+  ]
+});
+
+export default config;
