a13bfa2feac743b951efb01dff8c7202212c49d6
UnrelatedString <unrelatedstring@gmail.com>
2025-03-13 11:58:12 -0400

    favicon lmao

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,6 +2,7 @@ import type { RollupOptions } from 'rollup';
 import dev from 'rollup-plugin-dev';
 import html from '@rollup/plugin-html';
 import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 const config = (args: Record<string, any>): RollupOptions => ({
 	input: 'src/index.mjs',
@@ -10,6 +11,11 @@ const config = (args: Record<string, any>): RollupOptions => ({
 		format: 'es'
 	},
     plugins: [
+        copy({
+            targets: [
+                { src: 'static/**/*', dest: 'dist' }
+            ],
+        }),
         html({
             title: "bingous",
             meta: [
@@ -18,7 +24,12 @@ const config = (args: Record<string, any>): RollupOptions => ({
                 { property: 'og:title', content: 'bingous' },
                 { property: 'og:description', content: 'custom bingo cards, mostly for social media and stuff but like you can totally play bingo with them too if you want' },
                 { property: 'og:url', content: 'https://www.bingous.org' },
+                { property: 'og:image', content: 'https://www.bingous.org/bingous1.png' },
             ],
+            attributes: {
+                html: { lang: 'en' },
+                link: { rel: 'icon', href: 'favicon.ico' },
+            },
         }),
         dev({
             dirs: ["dist"],
