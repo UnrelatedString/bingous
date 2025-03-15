97f5935f0e3faad62854bd75087ab719f7aef8bd
UnrelatedString <unrelatedstring@gmail.com>
2025-03-15 03:04:36 -0400

    ooooooooooooh yeah I'm making it maybe almost actually react to things

--- src/Main.purs
+++ src/Main.purs
@@ -13,6 +13,8 @@ import Halogen.VDom.Driver (runUI)
 import CSS as CSS
 import CSS.Font as Font
 import Data.NonEmpty ((:|))
+import Data.Int (toNumber)
+import Web.CSSOM.MouseEvent (offsetX, offsetY)
 
 main :: Effect Unit
 main = HAff.runHalogenAff do
@@ -27,18 +29,24 @@ rootComponent =
     , eval: H.mkEval H.defaultEval { handleAction = handleAction }
     }
 
-type State = Unit
-type Action = Unit
+type Point = { x :: Number, y :: Number }
+
+type State = Point
+data Action = Click Point
 
 initialState :: forall input. input -> State
-initialState _ = unit
+initialState _ = { x: 0.0, y: 0.0 }
 
 handleAction :: forall output m. Action -> H.HalogenM State Action () output m Unit
-handleAction = pure
+handleAction (Click {x, y}) = H.modify_ $ const {x, y}
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render _ = HTML.div_
-  [ HTML.canvas [Prop.width 500, Prop.height 500]
+  [ HTML.canvas
+    [ Prop.width 500
+    , Prop.height 500
+    , Event.onClick \e -> Click { x: toNumber $ offsetX e, y: toNumber $ offsetY e }
+    ]
   , HTML.h2_ [HTML.text "wip :3"]
   , attribution
   ]
@@ -53,7 +61,7 @@ attribution = HTML.footer [style cutesyFooterStyle]
   [ HTML.p_ 
     [ HTML.text "made with "
     , HTML.b_ [HTML.text "halogen"]
-    , HTML.text " and sheer force of will. (c) UnrelatedString 2025. "
+    , HTML.text " and sheer force of will. (c) UnrelatedString 2025."
     ]
   , HTML.a [Prop.href "https://github.com/UnrelatedString/bingous"]
     [ HTML.text "github.com/UnrelatedString/bingous"
