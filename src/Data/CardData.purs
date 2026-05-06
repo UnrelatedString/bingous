1851b9888444e1b42e0311626c4beac7c0f31bfa
june \'unrelated string <june@sideros.rocks>
2026-05-06 17:34:41 -0400

    do not let the good be the enemy of the ANYTHING AT ALL

--- src/Data/CardData.purs
+++ src/Data/CardData.purs
@@ -1,6 +1,16 @@
 module Data.CardData
-  ( CardData(..)
+  ( CardData (..)
+  , FiveThings (..)
   )
 where
 
 newtype CardData = CardData {}
+
+-- can do dependent type shenanigans when I add support for non-5x5 grids :p
+newtype FiveThings a = FiveThings
+  { first :: a
+  , second :: a
+  , third :: a
+  , fourth :: a
+  , fifth :: a
+  }
